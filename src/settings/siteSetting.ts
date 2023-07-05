a9d2d8921e152ea0416d2659718047adf99894e3
wkzhang <8808994+dazaiqianyuan@user.noreply.gitee.com>
2023-07-05 08:21:40 +0800

    1

---
+++ src/settings/siteSetting.ts
@@ -0,0 +1,8 @@
+// github repo url
+export const GITHUB_URL = 'https://github.com/louiszwk/louisPnpm';
+
+
+export const DOC_URL = 'https://github.com/louiszwk/louisPnpm';
+
+// site url
+export const SITE_URL = 'https://github.com/louiszwk/louisPnpm';
